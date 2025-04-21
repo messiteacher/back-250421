62bfca87f1d592539f7212b3806d117a8928b351
ShinYounHo <suh2963@gmail.com>
2025-04-21 11:24:57 +0900

    init

---
+++ src/main/kotlin/com/example/back250421/Back250421Application.kt
@@ -0,0 +1,11 @@
+package com.example.back250421
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Back250421Application
+
+fun main(args: Array<String>) {
+    runApplication<Back250421Application>(*args)
+}
